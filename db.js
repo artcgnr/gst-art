7ed6bfc520419a8f2356e06ba6cc6f22192db7a4
artcgnr <artcgnrnidhi@gmail.com>
2026-08-18 10:47:29 +0530

    1st

--- db.js
+++ db.js
@@ -2,13 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/fireba
 import { getFirestore, collection, addDoc, getDoc, getDocs, query, where, orderBy, doc, setDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBA0eSQposFN_YhB4F3K2Q4uUzwSPcnZCc",
-    authDomain: "mytest-db-615d2.firebaseapp.com",
-    databaseURL: "https://mytest-db-615d2-default-rtdb.firebaseio.com",
-    projectId: "mytest-db-615d2",
-    storageBucket: "mytest-db-615d2.firebasestorage.app",
-    messagingSenderId: "7667456156",
-    appId: "1:7667456156:web:9daa4c020a486db62fb559"
+   apiKey: "AIzaSyBSyI6_dHpJCdQuTU9pdcCsvpLFUwhRnVc",
+    authDomain: "gst-billing-e17b3.firebaseapp.com",
+    projectId: "gst-billing-e17b3",
+    storageBucket: "gst-billing-e17b3.firebasestorage.app",
+    messagingSenderId: "110456904589",
+    appId: "1:110456904589:web:428c2be8fdd08c2662bbc4"
 };
 
 const app = initializeApp(firebaseConfig);
